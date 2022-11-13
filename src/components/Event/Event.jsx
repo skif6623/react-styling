eccc31a07941c41f8f5ec0cc3f873c14f66b6522
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-14 00:00:03 +0200

    d

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -5,33 +5,34 @@ import {
   FaClock,
 } from 'react-icons/fa';
 import PropTypes from 'prop-types';
-import css from './event.css';
+import css from './event.module.css';
 import { formatEventStart, formatEventDuration } from 'utils';
 import { iconSize } from 'constans';
+console.log(css);
 
 export function Event({ name, location, speaker, type, start, end }) {
   const formatedTime = formatEventStart(start);
   const formatedDuration = formatEventDuration(start, end);
   return (
     <div className={css.event}>
-      <h2 className="title">{name}</h2>
-      <p className="info">
-        <FaMapMarkerAlt className="icon" size={iconSize.sm} />
+      <h2 className={css.title}>{name}</h2>
+      <p className={css.info}>
+        <FaMapMarkerAlt className={css.icon} size={iconSize.sm} />
         {location}
       </p>
-      <p className="info">
-        <FaUserAlt className="icon" size={iconSize.sm} />
+      <p className={css.info}>
+        <FaUserAlt className={css.icon} size={iconSize.sm} />
         {speaker}
       </p>
-      <p className="info">
-        <FaCalendarAlt className="icon" size={iconSize.sm} />
+      <p className={css.info}>
+        <FaCalendarAlt className={css.icon} size={iconSize.sm} />
         {formatedTime}
       </p>
-      <p className="info">
-        <FaClock className="icon" size={iconSize.sm} />
+      <p className={css.info}>
+        <FaClock className={css.icon} size={iconSize.sm} />
         {formatedDuration}
       </p>
-      <span className="chip">{type}</span>
+      <span className={css.chip}>{type}</span>
     </div>
   );
   // body
