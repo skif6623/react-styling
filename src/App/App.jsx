7489ab25ce7187e7f38d5051375f1c25e3159fc8
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-14 20:44:08 +0200

    add emotion lib and the first emotion style

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,13 @@
 import { PageTitle } from 'components/PageTitle/PageTitle';
 import { EventBoard } from 'components/EventBoard/EventBoard';
 import upcomingEvents from 'upcoming-events';
+import { Container } from './App.styled';
 
 export function App() {
   return (
-    <div className="App">
+    <Container>
       <PageTitle text="24th core worlds coalition conference" />
       <EventBoard events={upcomingEvents} />
-    </div>
+    </Container>
   );
 }
---
+++ src/App/App.styled.jsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  padding: 150px;
+  background-color: green;
+`;
